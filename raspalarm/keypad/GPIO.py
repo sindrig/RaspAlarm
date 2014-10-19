7e8f2a2fb00f9acdddb1a61d3798885b89d4f34b
root <sindrigudmundsson@gmail.com>
2014-10-19 22:44:25 +0000

    Minimize helper script

--- raspalarm/keypad/GPIO.py
+++ raspalarm/keypad/GPIO.py
@@ -5,7 +5,6 @@
 # matrix_keypad on pypi: https://pypi.python.org/pypi/matrix_keypad
 
 import RPi.GPIO as GPIO
-#import wiringpi
 
 class Keypad():
     def __init__(self, columnCount = 3):
@@ -27,6 +26,12 @@ class Keypad():
                 self.KEYPAD[rp][rc] = KEYPAD[i][j]
 
     def getKey(self):
+        try:
+            return self._getKey()
+        finally:
+            self.exit()
+
+    def _getKey(self):
 
         # Set all columns as output low
         for pin in self.COLUMN:
@@ -38,14 +43,9 @@ class Keypad():
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # Scan rows for pushed key/button
-        rowSel = -1
-        for pin in self.ROW:
-            tmpRead = GPIO.input(pin)
-            if tmpRead == 0:
-                rowSel = pin
-
-        if not rowSel in self.ROW:
-            self.exit()
+        try:
+            rowSel = [pin for pin in self.ROW if GPIO.input(pin) == 0][0]
+        except IndexError:
             return
 
         # Convert columns to input
@@ -57,82 +57,30 @@ class Keypad():
         GPIO.output(rowSel, GPIO.HIGH)
 
         # Scan columns for still-pushed key/button
-        colSel = -1
-        for pin in self.COLUMN:
-            tmpRead = GPIO.input(pin)
-            if tmpRead == 1:
-                colSel = pin
-
-        if not colSel in self.COLUMN:
-            self.exit()
+        try:
+            colSel = [pin for pin in self.COLUMN if GPIO.input(pin) == 1][0]
+        except IndexError:
             return
 
         # Return the value of the key pressed
-        self.exit()
-        return self.KEYPAD[rowSel][colSel]
-
-    def _getKey(self):
-
-        # Set all columns as output low
-        for pin in self.COLUMN:
-            wiringpi.pinMode(pin, wiringpi.OUTPUT)
-            wiringpi.digitalWrite(pin, wiringpi.LOW)
-
-        # Set all rows as input
-        for pin in self.ROW:
-            wiringpi.pinMode(pin, wiringpi.INPUT)
-            wiringpi.pullUpDnControl(pin, wiringpi.PUD_UP)
-
-        # Scan rows for pushed key/button
-        rowSel = -1
-        for pin in self.ROW:
-            tmpRead = wiringpi.digitalRead(pin)
-            if tmpRead == 0:
-                rowSel = pin
-
-        if not rowSel in self.ROW:
-            self.exit()
-            return
-
-        # Convert columns to input
-        for pin in self.COLUMN:
-            wiringpi.pinMode(pin, wiringpi.INPUT)
-            wiringpi.pullUpDnControl(pin, wiringpi.PUD_DOWN)
-
-        # Switch the i-th row found from scan to output
-        wiringpi.pinMode(rowSel, wiringpi.OUTPUT)
-        wiringpi.digitalWrite(rowSel, wiringpi.HIGH)
-
-        # Scan columns for still-pushed key/button
-        colSel = -1
-        for pin in self.COLUMN:
-            tmpRead = wiringpi.digitalRead(pin)
-            if tmpRead == 1:
-                colSel = pin
-
-        if not colSel in self.COLUMN:
-            self.exit()
-            return
-
-        # Return the value of the key pressed
-        self.exit()
         return self.KEYPAD[rowSel][colSel]
 
     def exit(self):
         # Reinitialize all rows and columns as input at exit
         for pin in self.COLUMN + self.ROW:
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            # wiringpi.pinMode(pin, wiringpi.INPUT)
-            # wiringpi.pullUpDnControl(pin, wiringpi.PUD_UP)
 
 if __name__ == '__main__':
     # Initialize the keypad class
-    kp = keypad()
+    kp = Keypad()
 
     # Loop while waiting for a keypress
     digit = None
-    while digit == None:
-        digit = kp.getKey()
+    lastDigit = None
+    while 1:
+        while digit == None or digit == lastDigit:
+            digit = kp.getKey()
 
-    # Print the result
-    print digit
+        # Print the result
+        print digit
+        lastDigit = digit
