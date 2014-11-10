e7c9907b34056044876b3e8f4bad76fbf6e5eb2b
root <sindrigudmundsson@gmail.com>
2014-11-10 22:12:30 +0000

    Notify and send video. Also provide option to send simple messages

--- raspalarm/notify/__init__.py
+++ raspalarm/notify/__init__.py
@@ -1,13 +1,16 @@
 from raspalarm.conf import settings, getLogger
 
-__all__ = ['notify_motion', 'notify_video']
+__all__ = ['notify_motion', 'notify_video', 'notify_simple']
 
 logger = getLogger(__name__)
 
 if settings.PUSHBULLET_API_KEY:
-    from sender import notify_motion, notify_video
+    from sender import notify_motion, notify_video, notify_simple
 else:
-    def log(*args, **kwargs):
-        logger.debug('PUSHBULLET_API_KEY not defined')
-    notify_motion = log
-    notify_video = log
+    def log(name):
+        def do_log(*args, **kwargs):
+            logger.debug('PUSHBULLET_API_KEY not defined, %s called', name)
+        return do_log
+    notify_motion = log('notify_motion')
+    notify_video = log('notify_video')
+    notify_simple = log('notify_simple')
--- raspalarm/notify/sender.py
+++ raspalarm/notify/sender.py
@@ -10,15 +10,30 @@ pb = PushBullet(settings.PUSHBULLET_API_KEY)
 def notify_video(video_file=None):
     logger.debug('Sending video %s', video_file)
     if video_file:
-        pass
+        with open(video_file, 'rb') as f:
+            success, file_data = pb.upload_file(
+                f,
+                datetime.datetime.now().strftime('%H:%M.mp4')
+            )
+        success, push = pb.push_file(**file_data)
+        return success
     else:
-        notify_motion()
+        return notify_motion()
 
 def notify_motion():
     logger.debug('Sending motion notify!')
-    pb.push_note(
+    success, push = pb.push_note(
         'RaspAlarm: Motion detected!',
         'Motion detected at %s' % datetime.datetime.now().strftime(
             '%d.%m.%Y %H:%M:%S'
         )
     )
+    return success
+
+def notify_simple(text):
+    logger.debug('Sending message: %s', text)
+    success, push = pb.push_note(text, '')
+    return success
+
+if __name__ == '__main__':
+    notify_video('/mnt/ext/videos/motion_20141028084726.mp4')
